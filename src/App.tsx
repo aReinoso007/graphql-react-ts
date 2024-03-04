0f9e7c3dc318257f7768737a4abf4d7de2c634ef
Alex Reinoso <ralexjessiel@gmail.com>
2024-03-04 17:02:40 -0500

    search feature

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import CharacterPage from "./Pages/Character-page";
 import HomePage from "./Pages/Home-page";
+import SearchComponent from "./Components/Search";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/:id" element={<CharacterPage />} />
+      <Route path="/search" element={<SearchComponent />} />
       <Route path="*" element={<h1>Not Found</h1>} />
     </Routes>
   );
---
+++ src/Components/Search.tsx
@@ -0,0 +1,49 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useLazyQuery } from "@apollo/client";
+import { useState } from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import { GET_CHARACTERS_LOCATIONS } from "../Queries/Queries";
+
+const SearchComponent: React.FC = () => {
+  const [name, setName] = useState<string>("");
+
+  const [getLocations, { data, loading, error }] = useLazyQuery(
+    GET_CHARACTERS_LOCATIONS,
+    {
+      variables: { name },
+    }
+  );
+  const search = () =>{
+    getLocations();
+  }
+
+  return (
+    <Container>
+        <br />
+      <Row>
+        <Col>
+          <input
+            value={name}
+            type="text"
+            placeholder="Search..."
+            onChange={(event) => setName(event.target.value)}
+          />
+        </Col>
+        <Col>
+          <Button onClick={search}>Search</Button>
+        </Col>
+      </Row>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error.message}</p>}
+      {data && (
+        <ul>
+            {data.characters.results.map((character: any, index: any) => {
+                return <li key={index}>{character.location.name}</li>;
+            })}
+        </ul>
+      )}
+    </Container>
+  );
+};
+
+export default SearchComponent;
